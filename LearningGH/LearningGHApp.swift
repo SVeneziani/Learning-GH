eee788fc48391383fc521bd71c99e0303c7afe7b
SVeneziani <92427689+SVeneziani@users.noreply.github.com>
2021-11-30 21:46:36 +0100

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Sergio Veneziani on 30/11/21.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
